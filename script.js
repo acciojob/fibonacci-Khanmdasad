d66de81802f18b60d2937324746a1406e7251f08
Asadullah Khan <67080107+Khanmdasad@users.noreply.github.com>
2024-05-29 13:47:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function fibonacci(num) {
 // your code here
 	let t1 = 0, t2 = 1, t3;
+	if(num == 1){
+		return t1;
+	}
+	else if(num == 2){
+		return t2;
+	}
 	for(let i = 2; i < num; i++){
 		t3 = t1 + t2;
 		t1 = t2;
