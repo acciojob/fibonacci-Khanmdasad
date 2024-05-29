ac22e7f4899e9e5210857ac80764b07a30481eed
Asadullah Khan <67080107+Khanmdasad@users.noreply.github.com>
2024-05-29 13:31:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	let t1 = 0; y2 = 1; t3;
+	for(let i = 2; i < num; i++){
+		t3 = t1 + t2;
+		return t3;
+		t1 = t2;
+		t2 = t3;
+	}
 }
 
 module.exports = fibonacci;
